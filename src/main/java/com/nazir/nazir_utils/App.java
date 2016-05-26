eca0f20a497ad426471c45aa278b4595d56e3abc
luo <353830102@qq.com>
2016-05-26 16:35:55 +0800

    first commit

---
+++ src/main/java/com/nazir/nazir_utils/App.java
@@ -0,0 +1,13 @@
+package com.nazir.nazir_utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
