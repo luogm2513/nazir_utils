91840360390360265716bc39cd065999ddc11b85
luogm <353830102@qq.com>
2016-08-10 09:43:38 +0800

    add PinyinUtil

---
+++ src/main/java/com/nazir/utils/PinYinUtil.java
@@ -0,0 +1,85 @@
+package com.nazir.utils;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+
+/**
+ * @author luogm
+ *
+ */
+public class PinYinUtil {
+	/**
+	 * 提取每个汉字的首字母(大写)
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String getPinYinHeadChar(String str) {
+	    if (isNull(str)) {
+	        return "";
+	    }
+	    String convert = "";
+	    for (int j = 0; j < str.length(); j++) {
+	        char word = str.charAt(j);
+	        // 提取汉字的首字母
+	        String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+	        if (pinyinArray != null) {
+	            convert += pinyinArray[0].charAt(0);
+	        }
+	        else {
+	            convert += word;
+	        }
+	    }
+	
+	    convert = string2AllTrim(convert);
+	    return convert.toUpperCase();
+	}
+	
+    /**
+	 * 提取第一个汉字的首字母(大写)
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String getFirstHeadChar(String str) {
+	    if (isNull(str)) {
+	        return "";
+	    }
+	    String convert = "";
+	    char word = str.charAt(0);
+	    // 提取汉字的首字母
+	    String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+	    if (pinyinArray != null) {
+	        convert += pinyinArray[0].charAt(0);
+	    }
+	    else {
+	        convert += word;
+	    }
+	
+	    convert = string2AllTrim(convert);
+	    return convert.toUpperCase();
+	}
+	
+	/*
+	* 判断字符串是否为空
+	*/
+	
+	public static boolean isNull(Object strData) {
+	    if (strData == null || String.valueOf(strData).trim().equals("")) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	/**
+	* 去掉字符串包含的所有空格
+	* 
+	* @param value
+	* @return
+	*/
+	public static String string2AllTrim(String value) {
+	    if (isNull(value)) {
+	        return "";
+	    }
+	    return value.trim().replace(" ", "");
+	}
+}
